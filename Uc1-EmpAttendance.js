56ac7bf33aabaca67c5812c358393a94c3bf6058
Hemanthb <hemanthb93@gmail.com>
2022-07-06 11:11:38 +0530

    [Hemanth]:calculates daily wage of an employee based on work hrs

--- Uc1-EmpAttendance.js
+++ Uc1-EmpAttendance.js
@@ -1,10 +1,20 @@
 {
-    const IS_ABSENT = 0;
-    let random = Math.floor(Math.random() * 10) % 2;
-    if(random == IS_ABSENT)
-    {
-        console.log("Employee is Absent");
-        return;
+    const IS_ABSENT = 0, IS_PART_TIME = 1, IS_FULL_TIME = 2;
+    const WAGE_PER_HRS = 20;
+    let random = Math.floor(Math.random() * 10) % 3;
+    let empHrs = 0;
+    switch(random){
+        case IS_ABSENT:
+            console.log("Employee is Absent hence wage is Rs.0");
+            return;
+        case IS_PART_TIME:
+            empHrs = 4;
+            break;
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
     }
-    console.log("Employee is Present");
+    let empWage = empHrs * WAGE_PER_HRS;
+    console.log("Emp Wage for a day is -> Rs. "+empWage);
+    
 }
