d9fdf1fecddf69c4630074d633cc1fdbf67bd984
Hemanthb <hemanthb93@gmail.com>
2022-07-06 19:33:47 +0530

    [Hemanth]:Stores daily wage calculated in an array

--- Uc1-EmpAttendance.js
+++ Uc1-EmpAttendance.js
@@ -1,6 +1,7 @@
 const IS_ABSENT = 0, IS_PART_TIME = 1, IS_FULL_TIME = 2, TOTAL_WORKING_HRS = 160;
 const WAGE_PER_HRS = 20;
 const TOTAL_WORKING_DAYS = 20;
+let empDailyWageArray = new Array();
 {
     
     let totalWorkHrs = 0;
@@ -8,13 +9,18 @@ const TOTAL_WORKING_DAYS = 20;
     while(i < TOTAL_WORKING_DAYS && TOTAL_WORKING_HRS > totalWorkHrs){
         i++;
         let dayWorkHrs = GetWorkingHrs();
-        let DailyWage = WAGE_PER_HRS * dayWorkHrs;
+        let DailyWage = CalculateWage(dayWorkHrs);
         totalWorkHrs += dayWorkHrs;
         console.log("Day -"+i+" wage is - Rs. "+DailyWage);
+        empDailyWageArray.push(DailyWage);
+
     }
-    let empWage = WAGE_PER_HRS * totalWorkHrs;
+    let empWage = CalculateWage(totalWorkHrs);
     console.log("Emp Wage for a Month is -> Rs. "+empWage+" and total work hrs is "+totalWorkHrs+"hrs");
     
+}
+function CalculateWage(totalWorkingHrs){
+    return WAGE_PER_HRS*totalWorkingHrs;
 }
 function GetWorkingHrs(){
     let random = Math.floor(Math.random() * 10) % 3;
@@ -31,3 +37,10 @@ function GetWorkingHrs(){
             return 8;
     }
 }
+//using foreach to calculate total wage by iterating over daily wage array
+let totWage = 0;
+empDailyWageArray.forEach(wageCalculate);
+function wageCalculate(wage){
+    totWage += wage
+}
+console.log("Total wage calculated using sum of daily wage array --> Rs."+totWage);
