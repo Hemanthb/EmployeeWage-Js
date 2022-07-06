06ebb5a0e20b7e08b1097b638931fc1b27371250
Hemanthb <hemanthb93@gmail.com>
2022-07-06 11:38:30 +0530

    [hemanth]:calculates emp wage for a month

--- Uc1-EmpAttendance.js
+++ Uc1-EmpAttendance.js
@@ -1,9 +1,16 @@
 {
     var IS_ABSENT = 0, IS_PART_TIME = 1, IS_FULL_TIME = 2;
     const WAGE_PER_HRS = 20;
-    
-    let empWage = WAGE_PER_HRS * GetWorkingHrs();
-    console.log("Emp Wage for a day is -> Rs. "+empWage);
+    const TOTAL_WORKING_DAYS = 20;
+    let totalWorkHrs = 0;
+    for(let i =1;i<=TOTAL_WORKING_DAYS;i++){
+        let dayWorkHrs = GetWorkingHrs();
+        let DailyWage = WAGE_PER_HRS * dayWorkHrs;
+        totalWorkHrs += dayWorkHrs;
+        console.log("Day -"+i+" wage is - Rs. "+DailyWage);
+    }
+    let empWage = WAGE_PER_HRS * totalWorkHrs;
+    console.log("Emp Wage for a Month is -> Rs. "+empWage+" and total work hrs is "+totalWorkHrs+"hrs");
     
 }
 function GetWorkingHrs(){
