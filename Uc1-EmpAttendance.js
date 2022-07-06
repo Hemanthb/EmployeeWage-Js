1e64927337f745b80101bd54cd4470f5031ba746
Hemanthb <hemanthb93@gmail.com>
2022-07-06 11:22:51 +0530

    [Hemanth]:Employee hours id determined inside a function'

--- Uc1-EmpAttendance.js
+++ Uc1-EmpAttendance.js
@@ -1,20 +1,23 @@
 {
-    const IS_ABSENT = 0, IS_PART_TIME = 1, IS_FULL_TIME = 2;
+    var IS_ABSENT = 0, IS_PART_TIME = 1, IS_FULL_TIME = 2;
     const WAGE_PER_HRS = 20;
-    let random = Math.floor(Math.random() * 10) % 3;
-    let empHrs = 0;
-    switch(random){
-        case IS_ABSENT:
-            console.log("Employee is Absent hence wage is Rs.0");
-            return;
-        case IS_PART_TIME:
-            empHrs = 4;
-            break;
-        case IS_FULL_TIME:
-            empHrs = 8;
-            break;
-    }
-    let empWage = empHrs * WAGE_PER_HRS;
+    
+    let empWage = WAGE_PER_HRS * GetWorkingHrs();
     console.log("Emp Wage for a day is -> Rs. "+empWage);
     
+}
+function GetWorkingHrs(){
+    let random = Math.floor(Math.random() * 10) % 3;
+    
+    switch(random){
+        case IS_ABSENT:
+            console.log("Employee is Absent");
+            return 0;
+        case IS_PART_TIME:
+            console.log("Employee worked Part time.")
+            return 4;
+        case IS_FULL_TIME:
+            console.log("Employee worked Full time.")
+            return 8;
+    }
 }
