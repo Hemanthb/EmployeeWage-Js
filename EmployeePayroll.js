f85ceaef68104a69bc6e70d0246b84ffe2131ea8
Hemanthb <hemanthb93@gmail.com>
2022-07-07 23:23:37 +0530

    [Hemanth]:Adds new properties gender and startdate

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -2,12 +2,16 @@ class EmployeePayroll
 {
     id; 
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary)
+    constructor(...params)
     {
-        this.id = id;
-        this.name = name
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     get name() 
@@ -21,11 +25,16 @@ class EmployeePayroll
 
     ToString()
     {
-        return "Id: " + this.id + "\t\tEmpName: " + this.name + "\t\tSalary: " + this.salary;
+        return "Id: " + this.id + "\t\tEmpName: " + this.name + "\t\tSalary: " + this.salary +
+        "\t\tGender: " + this.gender + "\t\tStartDate: "+ this.startDate;
     }
 }
-let empPayroll = new EmployeePayroll(1,"Asha",25000);
+let empPayroll = new EmployeePayroll(1,"Asha",25000,"F");
 console.log(empPayroll.ToString());
 empPayroll.id = 2;
 empPayroll.name = "Bharathan";
+empPayroll.gender = "M";
 console.log(empPayroll.ToString());
+const date = new Date();
+let empPayroll1 = new EmployeePayroll(3, "Saneesh", 22000, "M", date.toLocaleDateString());
+console.log(empPayroll1.ToString());
